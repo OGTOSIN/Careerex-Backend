feab1681f712f061fde394186a72e76230777d27
Oluwatosin Oguntoyinbo <oluwatosinoguntoyinboo@gmail.com>
2025-04-05 20:00:54 +0100

    Day 1 - Week 3 Work

---
+++ Asset/Script/class-practice.js
@@ -0,0 +1,44 @@
+// Meet Oluwatosin
+
+// Strings
+var firstIntroToTheWorld = "Hello, World!";
+var firstIntroToTheClass = 'Hello, I am Oluwatosin!';
+var myFavouriteSongNowIs = `Ebinpami!`;
+var myGreatBackendInstructorIs = 'Mr D, I am sure you know!';
+var andTheReasonBeingThat = "He is Good at what he does and his style of teachng is superb!";
+
+// Numbers
+var myFavouriteNumberNowIs = 7; //RESPECT is Key!
+var numberIAmAimingFor = 100;   //Percent
+var wordAttitudeSumsUpTo = 100; // Try It!
+var numbersInTheAlphabetAre = 26;
+var theySayILoveYou = 143; //Ha Ha!
+
+// Booleans
+var areYouOpenToNetworking = true;
+var canTheyDirectMessageYou = true;
+var doYouHateExploringThings = false;
+var hmmGreatInstructorAndStudents = true;
+var yeahNoBadEnergyHere = true;
+
+// Boolean Output
+console.log(areYouOpenToNetworking);
+console.log(canTheyDirectMessageYou);
+console.log(doYouHateExploringThings);
+console.log(hmmGreatInstructorAndStudents);
+console.log(yeahNoBadEnergyHere);
+
+
+// console.log(firstIntroToTheWorld);
+// console.log(firstIntroToTheClass);
+// console.log(myFavouriteSongNowIs);
+// console.log(myGreatBackendInstructorIs);
+// console.log(andTheReasonBeingThat);
+
+// console.log(myFavouriteNumberNowIs);
+// console.log(numberIAmAimingFor);
+// console.log(wordAttitudeSumsUpTo);
+// console.log(numbersInTheAlphabetAre);
+// console.log(theySayILoveYou);
+
+
