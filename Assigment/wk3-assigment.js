dd129cd9c4bb54b758eaef81c2035bfb021333fc
Oluwatosin Oguntoyinbo <oluwatosinoguntoyinboo@gmail.com>
2025-04-11 15:52:38 +0100

    Wk3 Assignment in Progress

--- Assigment/wk3-assigment.js
+++ Assigment/wk3-assigment.js
@@ -31,30 +31,77 @@ console.log(introduction);
 
 // 2. Make a variable with your favorite quote or lyric. Change the entire text to uppercase and lowercase in separate steps.
 
+//My Favourite Quote at the time!
 let favoriteQuote = "You cannot keep doing things the same way and expect a different result!"
 
+//The output of my fav quote in upper case
 console.log(favoriteQuote.toUpperCase());
+
+//The output of my fav quote in lower case
 console.log(favoriteQuote.toLowerCase());
 
 
 
 // 3.Take a word and rearrange it in reverse order, then log the reversed word.
 
+const revWord = "Rearrange";
 
+// console.log(revWord.reverse())
 
 // 4.Store the price of 3 items in 3 different variables. Calculate the total and display it with a message.
+
+//My wishlist items
+laptop = 750000;
+smartPhone = 350000;
+miscExp = 1000000;
+
+// The addition of  total on my wishlist
+wishTotal = laptop + smartPhone + miscExp;
+
+// The output of the item's total on my wishlist
+console.log(`The total money needed is about ${wishTotal} to buy the items on my wishlist`) //ES6
+console.log("The total money needed is about " + wishTotal + " to buy the items on my wishlist") //ES5
 
 
 
 // 5.Find the average of 5 test scores stored in variables. Display the average with a message.
 
+//The list of 5  test scores
+mathTestScore = 75;
+englishTestScore = 85;
+accountTestScore = 80;
+infoTechTestScore = 100;
+biologyTestScore = 70;
+
+//The average of the Test Scores
+averageTestScore = (mathTestScore + englishTestScore + accountTestScore + infoTechTestScore + biologyTestScore) / 5
+
+// The output of the average test's score
+console.log(`The average score for the 5 test released so far is  ${averageTestScore}.`)
 
 
 // 6.Create an array of your 5 favorite foods. Display the first and last items in the array.
 
+//An array of my favourite delicious dishes
+favoriteFoods = ["Party Jollof", "Yam and Beans", "Pounded Yam and Egusi", "Yam and Garden Egg Sauce", "Fruit Salad with Vegetables"];
 
+//Display item 1 on the list of my favorite food
+console.log(favoriteFoods[0]);
+
+//Display item 5 on the list of my favorite food
+console.log(favoriteFoods[4]);
 
 // 7.Add two more food items to the list: one at the beginning and one at the end. Display the new list.
+
+//Adding a new food to the beginning of my fvourite list
+favoriteFoods.unshift("Beans and Plaintain");
+
+
+//Adding a new food to the end of my fvourite list
+favoriteFoods.push("Rice and Plaintain");
+
+// The output of the list after adding 2 more food to the list
+console.log(favoriteFoods);
 
 
 
